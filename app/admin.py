33d03484c496897b1eec96a1c443bd5953195121
HuerRome <4n.rooo@gmail.com>
2022-03-05 15:22:15 -0300

    register models in admin file

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
+from secrets import choice
+from unicodedata import category
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Choice)
